2cdaeae3dbc36f2a2533369060145edd321cfd97
Ante Kegalj <akegalj@gmail.com>
2024-06-24 19:55:47 +0200

    Pin servant packages

--- default.nix
+++ default.nix
@@ -15,9 +15,35 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+# TODO: see https://github.com/jonathanknowles/obelisk-reflex-servant-example/blob/master/default.nix
+#   packages = {
+#   };
+  overrides = self: super: {
+     servant = self.callHackageDirect {
+       pkg = "servant";
+       ver = "0.20.1";
+       sha256 = "wpvDsVJeq+ETvjMkCDyrZEjjYpeZO9Mddw7YAlEi1Wk=";
+     } {};
+     servant-client = self.callHackageDirect {
+       pkg = "servant-client";
+       ver = "0.20";
+       sha256 = "IajPlRqWP/zKmYU2oxnrIHre2DoAkYB5rW6fIyApYkc=";
+     } {};
+     servant-client-core = self.callHackageDirect {
+       pkg = "servant-client-core";
+       ver = "0.20";
+       sha256 = "KOW1AkFxtDnY73ONrTnvmsYUNATSbVq3/Arnv28P5Ho=";
+     } {};
+     servant-server = self.callHackageDirect {
+       pkg = "servant-server";
+       ver = "0.20";
+       sha256 = "krUEfIbGyqkCAX8ahRJCGnR4d45k1FbTz7NaXEbV0mc=";
+     } {};
+  };
 })
